3b881ef9de1ea2948ec4d11674d5e43356c49187
Iñigo <iallende2@gmail.com>
2015-04-16 23:49:31 +0200

    Añadido el bucle para comparar estimadores

--- SAD/src/sad/BNetwork.java
+++ SAD/src/sad/BNetwork.java
@@ -40,27 +40,52 @@ private static BNetwork miBNetwork = new BNetwork();
 		
 		BayesNet classifier;
 		Evaluation evaluator;
-
-		classifier = new BayesNet();
-		evaluator = new Evaluation(data);
+		double mejorFM = 0.0;
+		String mejorEst = null;
 		
 		
-		SimpleEstimator est = new SimpleEstimator();
-		//MultiNomialBMAEstimator est = new MultiNomialBMAEstimator();
+		SimpleEstimator sEst = new SimpleEstimator();
+		MultiNomialBMAEstimator est = new MultiNomialBMAEstimator();
 		//BMAEstimator est = new BMAEstimator();
+		ArrayList<BayesNetEstimator> estimadores = new ArrayList<>();
+		estimadores.add(new SimpleEstimator());
+		estimadores.add(new BMAEstimator());
+		estimadores.add(new MultiNomialBMAEstimator());
+		//Al usar este me sale un error de incorrect estimator use subclass
+		//estimadores.add(new BayesNetEstimator());
+		
+		ArrayList<String> lista = new ArrayList<>();
+		lista.add("Simple Estimator");
+		lista.add("BMA Estimator");
+		lista.add("MultiNomialBMA Estimator");
+		//Por cara estimador que vayamos a usar
+		for(int i = 0; i < estimadores.size(); i++)
+		{
+			System.out.println(i);
+			classifier = new BayesNet();
+			evaluator = new Evaluation(data);
+			classifier.setEstimator(estimadores.get(i));
+			
+			evaluator.crossValidateModel(classifier, data, 10, new Random(2));
+			double j = evaluator.weightedFMeasure();
+			if(j > mejorFM)
+			{
+				mejorFM = j;
+				mejorEst = lista.get(i);
+			}
+		}
+		
+		
 		est.setAlpha(0.5);
-		classifier.setEstimator(est);
-		//classifier.getEstimator().setAlpha(0.5);
 		//SearchAlgorithm as = new SearchAlgorithm();
 		K2 search = new K2();
 		//HillClimber search = new HillClimber();
 		search.setMaxNrOfParents(1);
 		
-		classifier.setSearchAlgorithm(search);
-		classifier.setUseADTree(false);
-		evaluator.crossValidateModel(classifier, data, 10, new Random(2));
-		double j = evaluator.weightedFMeasure();
-		System.out.println(j);
+		//classifier.setSearchAlgorithm(search);
+		//classifier.setUseADTree(false);
+		
+		System.out.println("El mejor estimador es: "+mejorEst+" con F-measure: "+mejorFM+".	");
 	}
 		//0.7038798522052109
 		//0.7038798522052109
